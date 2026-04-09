4d4f1da9728afb5854ab414750205605309b338c
Thomas Tran <109505566+ThomasTran808@users.noreply.github.com>
2026-04-08 15:47:00 -1000

    Display flyer information

--- src/components/FlyerDetailCard.tsx
+++ src/components/FlyerDetailCard.tsx
@@ -1,5 +1,5 @@
 import { Flyer } from '@prisma/client';
-import { CalendarEventFill, GeoAltFill, PersonFill, EnvelopeFill } from 'react-bootstrap-icons';
+import { CalendarEventFill, GeoAltFill } from 'react-bootstrap-icons';
 
 const FlyerDetailCard = ({ flyer }: { flyer: Flyer }) => (
   <div className="flyer-detail-note">
@@ -7,7 +7,6 @@ const FlyerDetailCard = ({ flyer }: { flyer: Flyer }) => (
     <div className="flyer-note-corner-rainbow" />
 
     <h1 className="flyer-detail-title">{flyer.title}</h1>
-    <span className="flyer-category-badge">{flyer.category}</span>
     <p className="flyer-detail-description">{flyer.description}</p>
 
     <div className="flyer-detail-info">
@@ -18,14 +17,6 @@ const FlyerDetailCard = ({ flyer }: { flyer: Flyer }) => (
       <div className="flyer-info-row">
         <GeoAltFill className="flyer-info-icon" />
         <span>{flyer.location}</span>
-      </div>
-      <div className="flyer-info-row">
-        <PersonFill className="flyer-info-icon" />
-        <span>{flyer.owner}</span>
-      </div>
-      <div className="flyer-info-row">
-        <EnvelopeFill className="flyer-info-icon" />
-        <span>{flyer.contactInfo}</span>
       </div>
     </div>
   </div>
