ae2caec055705d255a157bcf8b2c966408c1524c
Tamelab <TAMELAB@HAWAII.EDU>
2026-04-20 19:00:09 -1000

    Use updated Navbar from main

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { BoxArrowRight, Lock } from 'react-bootstrap-icons';
-import Image from 'next/image';
 
 const NavBar: React.FC = () => {
   const { data: session, status } = useSession();
@@ -19,43 +18,16 @@ const NavBar: React.FC = () => {
     <div>
       <div className="top-strip"></div>
 
-      <Navbar className="nav-main py-0" expand="md">
-        <Container fluid className="px-3 position-relative">
-          <Navbar.Brand href="/" className="me-0">
-          <Image src="/logo.png" height="60" width="100"alt="Bow-lletins" />
+      <Navbar className="nav-main d-flex justify-content-between align-items-center px-4 py-2" expand="md">
+        <Container>
+          <Navbar.Brand href="/">
+            <img src="/logo.png" width="200" alt="Bow-lletins" />
           </Navbar.Brand>
 
-          <Nav className="nav-center d-none d-md-flex">
-            <Nav.Link href="/" className="nav-link-custom" active={pathName === '/'}>
-              Home
-            </Nav.Link>
-
-            <Nav.Link href="/profile" className="nav-link-custom" active={pathName === '/profile'}>
-              Profile
-            </Nav.Link>
-
-            <NavDropdown title="Categories" className="nav-link-custom">
-              <NavDropdown.Item href="#">Jobs</NavDropdown.Item>
-              <NavDropdown.Item href="#">Internships</NavDropdown.Item>
-              <NavDropdown.Item href="#">Events</NavDropdown.Item>
-              <NavDropdown.Item href="#">Study Groups</NavDropdown.Item>
-              <NavDropdown.Item href="#">Social</NavDropdown.Item>
-              <NavDropdown.Item href="#">Clubs</NavDropdown.Item>
-            </NavDropdown>
-
-            <Nav.Link href="#" className="nav-link-custom">
-              About
-            </Nav.Link>
-
-            <Nav.Link href="#" className="nav-link-custom">
-              Contact
-            </Nav.Link>
-          </Nav>
-
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto d-md-none" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="nav-center d-flex d-md-none">
+            <Nav className="mx-auto nav-center d-none d-md-flex">
               <Nav.Link href="/" className="nav-link-custom" active={pathName === '/'}>
                 Home
               </Nav.Link>
@@ -86,17 +58,7 @@ const NavBar: React.FC = () => {
               </Nav.Link>
             </Nav>
 
-            <Nav className="nav-auth ms-md-auto mt-3 mt-md-0">
-              {currentUser && (
-                <>
-                  <Nav.Link id="add-stuff-nav" href="/add" active={pathName === '/add'}>
-                    Add Stuff
-                  </Nav.Link>
-                  <Nav.Link id="list-stuff-nav" href="/list" active={pathName === '/list'}>
-                    List Stuff
-                  </Nav.Link>
-                </>
-              )}
+            <Nav className="ms-auto align-items-center">
 
               {currentUser && role === 'ADMIN' && (
                 <Nav.Link id="admin-stuff-nav" href="/admin" active={pathName === '/admin'}>
@@ -115,14 +77,17 @@ const NavBar: React.FC = () => {
                     <BoxArrowRight className="me-2" />
                     Sign Out
                   </NavDropdown.Item>
-                  <NavDropdown.Item id="login-dropdown-change-password" href="/auth/change-password">
+                  <NavDropdown.Item
+                    id="login-dropdown-change-password"
+                    href="/auth/change-password"
+                  >
                     <Lock className="me-2" />
                     Change Password
                   </NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <div className="auth-buttons">
-                  <a href="Link /" className="btn btn-sm text-uh-green">
+                <div>
+                  <a href="/signin" className="btn btn-sm text-uh-green me-2">
                     Login
                   </a>
                   <a href="/auth/signup" className="btn btn-sm btn-outline-success">
